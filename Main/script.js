481de0d5a9126619309475ae54f1fb36f31a951a
Zion Yang <zionremnant@gmail.com>
2022-02-25 02:22:16 -0800

    mediaquery errors

--- Main/script.js
+++ Main/script.js
@@ -1,10 +1,4 @@
-var sandwich = document.querySelector('.header .nav-bar .nav-list .sandwich');
 var mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 var menu_item = document.querySelector('.header .nav-bar .nav-list ul li a');
 var header = document.querySelector('.header.container');
-
-sandwich.addEventListener('click',()=>{
-    sandwich.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
 
