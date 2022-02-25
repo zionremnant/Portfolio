f0348d1c3d0427b48c96f4282a4b3df27842ce60
Zion Yang <zionremnant@gmail.com>
2022-02-24 23:56:17 -0800

    updated contact

--- Main/script.js
+++ Main/script.js
@@ -1,8 +1,10 @@
 var sandwich = document.querySelector('.header .nav-bar .nav-list .sandwich');
 var mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+var menu_item = document.querySelector('.header .nav-bar .nav-list ul li a');
 var header = document.querySelector('.header.container');
 
 sandwich.addEventListener('click',()=>{
     sandwich.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+
